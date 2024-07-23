f46989714d4aa14c56c782acd4dbe918938003d5
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2024-07-23 15:18:50 +0700

    feat: Add nuxt-gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxt/ui', '@vueuse/motion/nuxt'],
+    modules: ['@nuxt/ui', '@vueuse/motion/nuxt', 'nuxt-gtag'],
+    devtools: { enabled: true },
+    gtag: {
+        id: 'G-BT46E9BQJE',
+    },
     app: {
         pageTransition: { name: 'page', mode: 'out-in' },
     },
-    devtools: { enabled: true },
     css: ['~/assets/css/tailwind.css'],
     postcss: {
         plugins: {
